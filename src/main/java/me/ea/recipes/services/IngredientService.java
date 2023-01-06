0964dc34152ee2ecb1809745d77f8de5cf7cbcfa
eduard023 <edo.avdzh@yandex.ru>
2023-01-07 01:39:44 +0300

    проверьте дз

--- src/main/java/me/ea/recipes/services/IngredientService.java
+++ src/main/java/me/ea/recipes/services/IngredientService.java
@@ -2,7 +2,12 @@ package me.ea.recipes.services;
 
 import me.ea.recipes.model.Ingredient;
 
+import java.util.List;
+
 public interface IngredientService {
     Ingredient addIngredient(String id, Ingredient ingredient);
     Ingredient getIngredient(String id);
+    Ingredient updateIngredient(String id, Ingredient ingredient);
+    Ingredient deleteIngredient(String id);
+    List<Ingredient> getAllIngredient();
 }
--- src/main/java/me/ea/recipes/services/impl/RecipeServiceImpl.java
+++ src/main/java/me/ea/recipes/services/impl/RecipeServiceImpl.java
@@ -1,13 +1,10 @@
 package me.ea.recipes.services.impl;
 
-import me.ea.recipes.model.Ingredient;
 import me.ea.recipes.model.Recipe;
 import me.ea.recipes.services.RecipeService;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -30,4 +27,32 @@ public class RecipeServiceImpl implements RecipeService {
             throw new RuntimeException("id не найден");
         }
     }
+
+    @Override
+    public Recipe updateRecipe(String id, Recipe recipe) {
+        if (recipeMap.containsKey(id)){
+            return recipeMap.put(id, recipe);
+        }else {
+            throw new RuntimeException("id не найден");
+        }
+    }
+
+    @Override
+    public Recipe deleteRecipe(String id) {
+        if (recipeMap.containsKey(id)){
+            return recipeMap.remove(id);
+        }else {
+            throw new RuntimeException("id не существует");
+        }
+    }
+
+    @Override
+    public List<Recipe> getAllRecipe() {
+        List<Recipe> s = new ArrayList<>();
+        for (Map.Entry<String, Recipe> recipeEntry : recipeMap.entrySet()){
+            s.add(recipeEntry.getValue());
+        }
+        return s;
+    }
+
 }

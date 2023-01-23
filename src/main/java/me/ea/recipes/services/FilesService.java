2be07ec16bcbef568889a24ef8c28dfdb4df0c51
eduard023 <edo.avdzh@yandex.ru>
2023-01-23 22:16:28 +0300

    проверьте дз

--- src/main/java/me/ea/recipes/services/FilesService.java
+++ src/main/java/me/ea/recipes/services/FilesService.java
@@ -1,11 +1,22 @@
 package me.ea.recipes.services;
 
+import java.io.File;
+
 public interface FilesService {
     boolean saveToFile(String json);
 
     String readFromFiles();
 
+    boolean cleanDataFile();
+
+    //Файловый сервис для рецепта
+    boolean cleanRecipeDataFile();
+
     String readFromRecipeFiles();
 
     boolean saveToRecipeFile(String json);
+
+    File getRecipeDataFile();
+
+    File getIngredientDataFile();
 }
